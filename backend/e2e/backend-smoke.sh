2480f1c7c621ed80537f558c4ba3255ccadee0d6
Mahmoud <mahmoudnasser1561@gmail.com>
2026-01-22 00:31:04 +0200

    Final Test

--- backend/e2e/backend-smoke.sh
+++ backend/e2e/backend-smoke.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-for i in {1..10}; do
-    RESPONSE=$(curl -s --fail ${BACKEND_URL}/hash/test)
-    if echo "$RESPONSE" | grep -q "098f6bcd4621d373cade4e832627b4f6"; then
-        echo "Success!"
-        exit 0
-    else
-        echo "Attempt $i: Not ready yet, retrying..."
-        sleep 10
-    fi
-done
-
-echo "Correct hash not found after retries!"
-exit 1
+RESPONSE=$(curl -s --fail $BACKEND_URL/hash/test)
+if echo "$RESPONSE" | grep -q "098f6bcd4621d373cade4e832627b4f6"; then
+    echo "Success!"
+    exit 0
+else
+    echo "Error"
+    exit 1
+fi
